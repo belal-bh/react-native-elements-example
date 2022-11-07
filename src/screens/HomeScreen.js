c9a8e41f32ce68195d2f46eeb4b9a78d968ea863
Md. Belal Hossain <belal.cseai@gmail.com>
2022-11-07 20:06:15 +0600

    adds Text element

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,50 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View} from 'react-native';
-import {Button, Text} from '@rneui/base';
+import {View, FlatList, TouchableOpacity} from 'react-native';
+import {Button, Text, ListItem} from '@rneui/base';
+
+const screens_data = [
+  {
+    name: 'Typography',
+    description: 'Example of Text, View etc.',
+  },
+];
 
 export default HomeScreen = () => {
   const navigation = useNavigation();
+
+  const renderItem = ({item}) => (
+    <ListItem bottomDivider>
+      <ListItem.Content>
+        <TouchableOpacity onPress={() => navigation.navigate(item.name)}>
+          <ListItem.Title style={{color: '#00a0db', fontWeight: 'bold'}}>
+            {item.name}
+          </ListItem.Title>
+        </TouchableOpacity>
+        {item?.description && (
+          <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+        )}
+      </ListItem.Content>
+      <ListItem.Chevron />
+    </ListItem>
+  );
+
   return (
     <View>
-      <Text>HomeScreen</Text>
-      <Button onPress={() => navigation.navigate('Typography')}>
-        Typography
-      </Button>
+      <Text
+        h4={true}
+        h4Style={{
+          paddingVertical: 10,
+          justifyContent: 'center',
+          alignSelf: 'center',
+        }}>
+        React Native Elements Example
+      </Text>
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        data={screens_data}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
--- src/screens/TypographyScreen.js
+++ src/screens/TypographyScreen.js
@@ -5,7 +5,46 @@ import {Text} from '@rneui/base';
 export default TypographyScreen = () => {
   return (
     <View>
-      <Text>Typography Example!</Text>
+      <Text
+        h1={true}
+        h1Style={{
+          paddingVertical: 10,
+          paddingHorizontal: 5,
+          borderBottomColor: 'gray',
+          borderBottomWidth: 1,
+        }}>
+        This is h1 Text example.
+      </Text>
+      <Text
+        h2={true}
+        h2Style={{
+          paddingVertical: 10,
+          paddingHorizontal: 5,
+          borderBottomColor: 'gray',
+          borderBottomWidth: 1,
+        }}>
+        This is h2 Text example.
+      </Text>
+      <Text
+        h3={true}
+        h3Style={{
+          paddingVertical: 10,
+          paddingHorizontal: 5,
+          borderBottomColor: 'gray',
+          borderBottomWidth: 1,
+        }}>
+        This is h3 Text example.
+      </Text>
+      <Text
+        h4={true}
+        h4Style={{
+          paddingVertical: 10,
+          paddingHorizontal: 5,
+          borderBottomColor: 'gray',
+          borderBottomWidth: 1,
+        }}>
+        This is h4 Text example.
+      </Text>
     </View>
   );
 };
